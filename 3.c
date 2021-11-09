2a4ba1bc4856e266c87acc7a25d181ec2fef8c06
Nikaido-bit <93205099+Nikaido-bit@users.noreply.github.com>
2021-11-09 13:50:36 +0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,24 @@
+\\汉诺塔
+#include<stdio.h>
+void move(char x,char y)
+{
+  printf("%c-->%c\n", x, y");
+}
+void h(int x,char one,char two,char three)      
+{
+  if(x==1) move(one,two);
+  else 
+  {
+    h(x-1,one,two,three);
+    move(o, e);
+		h(x - 1, t, o, e);
+  }
+}    
+int main()
+{
+  int a;
+  printf("输入圆盘的数量：");
+  scanf("%d",&a);
+  h(a,'A','B','C');
+  return 0;
+}
