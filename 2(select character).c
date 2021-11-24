dfef33f61a9a5f35010d5327687981fea8a60de0
Nikaido-bit <93205099+Nikaido-bit@users.noreply.github.com>
2021-11-24 21:55:22 +0800

    Update 2(select character).c

--- 2(select character).c
+++ 2(select character).c
@@ -1,4 +1,4 @@
-\\输入一个字符，再输入一个以回车结束的字符串(少于80 个字符)，在字符串中查找该字符｡如果找到，则输出该字符在字符串中所对应的最大下标(下标从0 开始)；否则输出"Not Found"
+//输入一个字符，再输入一个以回车结束的字符串(少于80 个字符)，在字符串中查找该字符｡如果找到，则输出该字符在字符串中所对应的最大下标(下标从0 开始)；否则输出"Not Found"
 #include<stdio.h>
 int main()
 {
@@ -7,7 +7,7 @@ int main()
 	printf("Input a character:");
 	scanf("%c", &b);
 	printf("Input a string:");
-	scanf("%s", a);\\a为字符数组的首地址名
+	scanf("%s", a);//a为字符数组的首地址名
 	for (i = 0; a[i] != '\0'; i++)
 	if (a[i] == b) x = i;
 	if (x > 0) printf("index=%d\n", x);
